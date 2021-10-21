5e95e3b4452756cb7e7c6f2201f230ec6f108b67
Rdsarkar <rajapscl@gmail.com>
2021-10-21 13:28:35 +0600

    alll input done

--- src/app/component/input-details/input-details.component.ts
+++ src/app/component/input-details/input-details.component.ts
@@ -4,6 +4,7 @@ import { Input2Model } from './model/input2.model';
 import { InputsModel } from './model/inputs.model';
 import { Res } from './model/res.model';
 import { InputServiceService } from './service/input-service.service';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-input-details',
@@ -11,28 +12,38 @@ import { InputServiceService } from './service/input-service.service';
   styleUrls: ['./input-details.component.css']
 })
 export class InputDetailsComponent implements OnInit {
-  
+
 
   constructor(private inputServiceService: InputServiceService) { }
   examlevel: Input1Model[] = [];
-  ggggqdgwgwkegjh: Input2Model[]=[]
-  session: InputsModel[]=[]
+  ggggqdgwgwkegjh: Input2Model[] = []
+  session: InputsModel[] = [];
+  amarYear: number = new Date().getFullYear();
   ngOnInit(): void {
     this.ggggqdgwgwkegjh = this.inputServiceService.Einpu2();
     this.session = this.inputServiceService.Einputs();
     this.inputServiceService.Einput().subscribe(
-      (alvi)=>{
+      (alvi) => {
         console.log("test");
         console.log(alvi);
-        
-        
-          this.examlevel = alvi.payload;
+
+
+        this.examlevel = alvi.payload;
       },
-      (dip)=>{
+      (dip) => {
         console.log("error hoise");
-        
+
       }
-    )
+    );
+  }
+
+  amarCode(): number[] {
+    let i: number;
+    let j: number[] =[];
+    for (i = this.amarYear; i >= this.amarYear - 20; i--) {
+      j.push(i);
+    }
+    return j;
   }
 
 }
